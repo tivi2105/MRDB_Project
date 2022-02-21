97f8e7817ba861a6c475d6333b98f28ec11a3a9c
tivi2105 <tivi2105@gmail.com>
2022-02-20 23:09:41 -0800

    First commit

---
+++ src/main/java/com/mrdb/MrdbVersion1Application.java
@@ -0,0 +1,13 @@
+package com.mrdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrdbVersion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrdbVersion1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/mrdb/controller/MessageController.java
@@ -0,0 +1,16 @@
+package com.mrdb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "This is MRDB Application";
+    }
+
+}
