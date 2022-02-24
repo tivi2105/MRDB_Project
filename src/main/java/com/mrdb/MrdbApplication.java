ef53685aec28c16d32263bb51a4a76171010968f
tivi2105 <tivi2105@gmail.com>
2022-02-24 15:39:38 -0800

    Reqired changes are made

--- src/main/java/com/mrdb/MrdbApplication.java
+++ src/main/java/com/mrdb/MrdbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MrdbVersion1Application {
+public class MrdbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MrdbVersion1Application.class, args);
+		SpringApplication.run(MrdbApplication.class, args);
 	}
 
 }
